a3d1618ec3714fa049e6f5862138dd5a7828c395
Daniel Yang <91350928+sprwoo@users.noreply.github.com>
2024-08-11 18:35:55 -0400

    michale paid

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -7,7 +7,7 @@ const year = d.getFullYear();
 const calvin = [10, 2024];
 const omar = [4, 2024];
 const web = [1, 2024];
-const michael = [6, 2024];
+const michael = [12, 2024];
 const yeram = [10, 2023];
 
 
